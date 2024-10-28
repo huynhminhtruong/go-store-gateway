bee8f7ed65b4d23df9e1b07a4d31f037128d2a3e
huynhminhtruong <huynhminhtruong2003@gmail.com>
2024-10-29 01:38:40 +0700

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,47 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	"github.com/huynhminhtruong/go-store-gateway/config"
+	"github.com/huynhminhtruong/go-store-services/book-service/src/biz/application/core/domain"
 	"github.com/huynhminhtruong/go-store-services/book-service/src/services/book"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Adapter struct {
-	storing book.BookClient // This comes from generated Go source
+	bookSrv book.BookClient // This comes from generated Go source
 }
 
-// func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
-// 	var opts []grpc.DialOption                                                    // Data model for connection configurations
-// 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials())) // This is for disabling TLS
-// 	conn, err := grpc.NewClient(paymentServiceUrl, opts...)                       // Connect to service
+func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
+	var opts []grpc.DialOption                                                    // Data model for connection configurations
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials())) // This is for disabling TLS
+	conn, err := grpc.NewClient(paymentServiceUrl, opts...)                       // Connect to service
 
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer conn.Close()                 // Always close the connection before quitting the function
-// 	client := book.NewBookClient(conn) // Initializes the new payment stub instance
-// 	return &Adapter{storing: client}, nil
-// }
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()                 // Always close the connection before quitting the function
+	client := book.NewBookClient(conn) // Initializes the new payment stub instance
+	return &Adapter{bookSrv: client}, nil
+}
 
-// func (a *Adapter) Charge(order *domain.Order) error {
-// 	_, err := a.payment.Create(context.Background(), &payment.CreatePaymentRequest{
-// 		UserId:     order.CustomerID,
-// 		OrderId:    order.ID,
-// 		TotalPrice: order.TotalPrice(),
-// 	})
-// 	return err
-// }
+func (a *Adapter) AddBook(data *domain.Book) error {
+	_, err := a.bookSrv.Create(context.Background(), &book.CreateBookRequest{
+		Title:       data.Title,
+		Author:      data.Author,
+		PublishYear: data.PublishYear,
+	})
+	return err
+}
 
 func main() {
-	// bookAdapter, err := NewAdapter(config.GetBookServiceUrl()) // The payment endpoint is available on the config object
-	// if err != nil {
-	// 	log.Fatalf("Failed to initialize payment stub. Error: %v", err) // The Order service will not run without the payment config
-	// }
+	_, err := NewAdapter(config.GetBookServiceUrl()) // The payment endpoint is available on the config object
+	if err != nil {
+		log.Fatalf("Failed to initialize payment stub. Error: %v", err) // The Order service will not run without the payment config
+	}
 	// application := api.NewApplication(dbAdapter, paymentAdapter) // The payment adapter is now a must-have parameter
 	// grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
 	// grpcAdapter.Run()
--- go.mod
+++ go.mod
@@ -2,13 +2,15 @@ module github.com/huynhminhtruong/go-store-gateway
 
 go 1.23.1
 
-require github.com/huynhminhtruong/go-store-services/book-service v0.0.0-20241028150533-12a66b481e08
+require (
+	github.com/huynhminhtruong/go-store-services/book-service v0.0.0-20241028181749-6734f4bc9256
+	google.golang.org/grpc v1.67.1
+)
 
 require (
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
-	google.golang.org/grpc v1.67.1 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
 )
