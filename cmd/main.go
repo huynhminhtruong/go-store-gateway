87ad84690edb3ca224999c85432d0b52a8f80103
huynhminhtruong <huynhminhtruong2003@gmail.com>
2024-10-30 17:36:58 +0700

    init a http server to handle grpc-gateway

--- cmd/main.go
+++ cmd/main.go
@@ -3,56 +3,34 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
+	"os"
 
-	"github.com/huynhminhtruong/go-store-gateway/config"
-	"github.com/huynhminhtruong/go-store-services/book-service/src/biz/application/core/domain"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/huynhminhtruong/go-store-services/book-service/src/services/book"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-const (
-	BOOK_IP_KEY   = "BOOK_IP"
-	BOOK_PORT_KEY = "BOOK_PORT"
-)
-
-type Adapter struct {
-	bookSrv book.BookClient // This comes from generated Go source
-}
-
-func NewAdapter(bookServiceUrl string) (*Adapter, error) {
-	// Data model for connection configurations
-	var opts []grpc.DialOption
-	// This is for disabling TLS
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	// Connect to service
-	conn, err := grpc.NewClient(bookServiceUrl, opts...)
-
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()                 // Always close the connection before quitting the function
-	client := book.NewBookClient(conn) // Initializes the new book stub instance
-	return &Adapter{bookSrv: client}, nil
-}
-
-func (a *Adapter) AddBook(data *domain.Book) error {
-	_, err := a.bookSrv.Create(context.Background(), &book.CreateBookRequest{
-		Title:       data.Title,
-		Author:      data.Author,
-		PublishYear: data.PublishYear,
-	})
-	return err
-}
-
 func main() {
-	_, err := NewAdapter(config.GetServiceURL(BOOK_IP_KEY, BOOK_PORT_KEY))
-	if err != nil {
-		log.Fatalf("Failed to initialize book stub. Error: %v", err)
+	// get gRPC server endpoint from environment variables
+	grpcServerEndpoint := os.Getenv("BOOK_GRPC_SERVER_ENDPOINT")
+	if grpcServerEndpoint == "" {
+		log.Fatal("BOOK_GRPC_SERVER_ENDPOINT environment variable is not set")
 	}
-	// application := api.NewApplication(dbAdapter, paymentAdapter) // The payment adapter is now a must-have parameter
-	// grpcAdapter := grpc.NewAdapter(application, config.GetApplicationPort())
-	// grpcAdapter.Run()
-	// bookAdapter.storing
-	log.Println("gateway-service is running")
+
+	// creat HTTP router to get request HTTP
+	mux := runtime.NewServeMux()
+
+	// make a connection to gRPC server
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := book.RegisterBookServiceHandlerFromEndpoint(context.Background(), mux, grpcServerEndpoint, opts)
+	if err != nil {
+		log.Fatalf("Failed to start HTTP server: %v", err)
+	}
+
+	log.Println("Starting HTTP server on :8081")
+	if err := http.ListenAndServe(":8081", mux); err != nil {
+		log.Fatalf("Failed to serve HTTP server: %v", err)
+	}
 }
